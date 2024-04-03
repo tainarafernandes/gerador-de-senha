2838dc1cefb2bc356c0482682d64c5ade89415cd
tainarafernandes <125694417+tainarafernandes@users.noreply.github.com>
2024-04-03 13:58:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -58,17 +58,21 @@ function geraSenha(){
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
-    classificaSenha();
+    classificaSenha(alfabeto.length);
     
 }
 
-function classificaSenha(){
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.log(entropia);
     forcaSenha.classList.remove('fraca','media','forte');
-        if (tamanhoSenha > 11){
+        if (entropia > 57){
            forcaSenha.classList.add('forte');
-        } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+        } else if (entropia > 35 && tamanhoSenha < 57) {
             forcaSenha.classList.add('media');
-        } else if (tamanhoSenha <= 5){
+        } else if (entropia <= 35){
             forcaSenha.classList.add('fraca');
         }
+        const valorEntropia = document.querySelector('.entropia');
+        valorEntropia.textContent = "Um computador pode levar até " + Math.floor (2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }   
